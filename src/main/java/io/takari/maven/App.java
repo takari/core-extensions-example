f628ad33a97ad2059e3db8a9b5fc466d7afba6cc
Jason van Zyl <jason@tesla.io>
2015-03-31 15:14:48 -0400

    Core extensions example

---
+++ src/main/java/io/takari/maven/App.java
@@ -0,0 +1,13 @@
+package io.takari.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
